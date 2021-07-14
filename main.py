4d64d859ab30dbd60862a878b8137592d9711e74
Harrison Rose <harrisonmrose@gmail.com>
2021-07-13 23:26:14 -0400

    added best fit line, fixed legend points, played with subplots

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import requests
 from matplotlib.animation import FuncAnimation
-import matplotlib.lines as mlines
-from matplotlib.collections import PathCollection
 
 
 def give_me_a_straight_line(xs, ys, ws):
@@ -86,54 +84,19 @@ class GenAnimation:
     days = 180
     step = 1
 
-    def __init__(self, covid_data):
-        self.covid_data = covid_data
+    def __init__(self, stream):
         self.fig, self.ax = plt.subplots()
-
-        base = datetime.today()
-        date_list = [base - timedelta(days=x) for x in range(GenAnimation.days, 0, -GenAnimation.step)]
-        self.dates = [date.strftime("%Y-%m-%d") for date in date_list]
-
-        self.stream = list(self.data_stream())
-
+        self.stream = stream
         self.ani = FuncAnimation(self.fig,
                                  self.update,
                                  frames=GenAnimation.days // GenAnimation.step,
                                  init_func=self.setup_plot,
                                  blit=True)
 
-    def data_stream(self):
-        for date in self.dates:
-
-            # x-axis values
-            xs = [county["data"][date]["vaccinationsCompletedRatio"] * 100
-                  for county in self.covid_data
-                  if county.get("data", {date: None}).get(date)]
-
-            # y-axis values
-            ys = [county["data"][date]["caseDensity"]
-                  for county in self.covid_data
-                  if county.get("data", {date: None}).get(date)]
-
-            # size
-            ss = [max(1, county["population"] / 10000) + 2
-                  for county in self.covid_data
-                  if county.get("data", {date: None}).get(date)]
-
-            # weights
-            ws = [county["population"]
-                  for county in self.covid_data
-                  if county.get("data", {date: None}).get(date)]
-
-            cs = [county["color"]
-                  for county in self.covid_data
-                  if county.get("data", {date: None}).get(date)]
-            yield xs, ys, ss, cs, ws
-
     def setup_plot(self):
         """Initial drawing of the scatter plot."""
         x, y, s, c, w = self.stream[0]
-        self.ax.axis([5, 100, 0, 125])
+        self.ax.axis([0, 100, 0, 125])
 
         # x-axis label
         plt.xlabel('fully vaccinated %')
@@ -144,46 +107,67 @@ class GenAnimation:
         # showing legend
 
         # best line fit
-        line = give_me_a_straight_line(x, y, w)
-        # best = self.ax.plot(x, line, 'r--', label='best fit trend-line')
+        ys = give_me_a_straight_line(x, y, w)
+        self.line, = self.ax.plot(x, ys, 'r--', label='best fit trend-line')
 
         data = list(zip(x, y, s, c, w))
         # pick specific points to add to the legend
         dem = [point for point in data if point[3] == min(c)][0]
+        print(data.index(dem))
         rep = [point for point in data if point[3] == max(c)][0]
+        print(data.index(rep))
         _, mid_c = min([(abs(0.5 - color[0]), color) for color in c if color[1] == 0])
         neutral = [point for point in data if point[3] == mid_c][0]
+        print(data.index(neutral))
         unknown = [point for point in data if point[3] == (0.5, 0.5, 0.5)][0]
+        print(data.index(unknown))
 
         # add points to legend
-        dems = self.ax.scatter([dem[0]], [dem[1]], [dem[2]], [dem[3]],
+        self.dems = self.ax.scatter([dem[0]], [dem[1]], [dem[2]], [dem[3]],
                                label="pop: {}0K, dem".format(int(dem[2]-2)))
-        reps = self.ax.scatter([rep[0]], [rep[1]], [rep[2]], [rep[3]],
+        self.reps = self.ax.scatter([rep[0]], [rep[1]], [rep[2]], [rep[3]],
                                label="pop: {}0K, rep".format(int(rep[2]-2)))
-        neutrals = self.ax.scatter([neutral[0]], [neutral[1]], [neutral[2]], [neutral[3]],
+        self.neutrals = self.ax.scatter([neutral[0]], [neutral[1]], [neutral[2]], [neutral[3]],
                                    label="pop: {}0K, neutral".format(int(neutral[2]-2)))
-        unknowns = self.ax.scatter([unknown[0]], [unknown[1]], [unknown[2]], [unknown[3]],
+        self.unknowns = self.ax.scatter([unknown[0]], [unknown[1]], [unknown[2]], [unknown[3]],
                                    label="pop: {}0K, unknown".format(int(unknown[2]-2)))
 
         self.scat = self.ax.scatter(x, y, s=s, c=c)
-        plt.legend()
+        self.legend = plt.legend()
         # For FuncAnimation's sake, we need to return the artist we'll be using
         # Note that it expects a sequence of artists, thus the trailing comma.
-        return self.scat,
+        return self.scat, self.line, self.dems, self.reps, self.neutrals, self.unknowns
 
     def update(self, frame):
         x, y, s, c, w = self.stream[frame]
+
+        # data = list(zip(x, y, s, c, w))
+        # dem = [point for point in data if point[3] == min(c)][0]
+        # dem_index = data.index(dem)
+        # rep = [point for point in data if point[3] == max(c)][0]
+        # rep_index = data.index(rep)
+        # _, mid_c = min([(abs(0.5 - color[0]), color) for color in c if color[1] == 0])
+        # neutral = [point for point in data if point[3] == mid_c][0]
+        # neutral_index = data.index(neutral)
+        # unknown = [point for point in data if point[3] == (0.5, 0.5, 0.5)][0]
+        # unknown_index = data.index(unknown)
+        # self.scat.set_offsets(np.c_[x[dem_index], y[dem_index]])
+        # self.scat.set_sizes(np.array(s[dem_index]))
+        # self.scat.set_color(np.array(c[dem_index]))
+        # self.fig.legend(handles=(self.scat,),
+        #                 labels=("pop: {}0K, dem".format(int(dem[2] - 2)),))
+
         self.scat.set_offsets(np.c_[x, y])
         self.scat.set_sizes(np.array(s))
         self.scat.set_color(np.array(c))
 
         # best line fit
         ys = give_me_a_straight_line(x, y, w)
-        line = mlines.Line2D(x, ys)  # , 'r--')
-        transform = self.ax.transAxes
-        line.set_transform(transform)
-        # self.ax.add_line(line)
-        return self.scat,
+        self.line.set_xdata(x)
+        self.line.set_ydata(ys)
+        self.line.set_label('best fit trend-line')
+
+        return self.scat, self.line
 
 
 def add_election_info_to_covid_data(covid_data, election_data):
@@ -239,6 +223,42 @@ def process_election_data(election_data):
     return county_data
 
 
+def gen_plot_data(covid_data):
+    base = datetime.today()
+    date_list = [base - timedelta(days=x) for x in range(GenAnimation.days, 0, -GenAnimation.step)]
+    dates = [date.strftime("%Y-%m-%d") for date in date_list]
+
+    def data_stream():
+        for date in dates:
+
+            # x-axis values
+            xs = [county["data"][date]["vaccinationsCompletedRatio"] * 100
+                  for county in covid_data
+                  if county.get("data", {date: None}).get(date)]
+
+            # y-axis values
+            ys = [county["data"][date]["caseDensity"]
+                  for county in covid_data
+                  if county.get("data", {date: None}).get(date)]
+
+            # size
+            ss = [max(1, county["population"] / 10000) + 2
+                  for county in covid_data
+                  if county.get("data", {date: None}).get(date)]
+
+            # weights
+            ws = [county["population"]
+                  for county in covid_data
+                  if county.get("data", {date: None}).get(date)]
+
+            cs = [county["color"]
+                  for county in covid_data
+                  if county.get("data", {date: None}).get(date)]
+            yield xs, ys, ss, cs, ws
+
+    return list(data_stream())
+
+
 def main():
 
     election_data = get_processed_election_data()
@@ -251,12 +271,34 @@ def main():
     save_covid_data(covid_data)
 
     add_election_info_to_covid_data(covid_data, election_data)
+    stream_data = gen_plot_data(covid_data)
 
-    a = GenAnimation(covid_data)
+    # give a array of subplots for data going back in time
+    # fig = plt.figure()
+    # rows = 3
+    # cols = 3
+    # step_days = 14
+    # gs = fig.add_gridspec(rows, cols, hspace=0, wspace=0, left=0.05, right=0.95, bottom=0.05, top=0.95)
+    # axs = gs.subplots(sharex=True, sharey=True)
+    # for i in range(rows):
+    #     for j in range(cols):
+    #         index = - 1 - step_days * i * cols - step_days * j
+    #         print(i, j, index)
+    #         axs[i, j].label_outer()
+    #         axs[i, j].axis([0, 100, 0, 150])
+    #         axs[i, j].scatter(
+    #             stream_data[index][0],
+    #             stream_data[index][1],
+    #             s=stream_data[index][2],
+    #             c=stream_data[index][3])
+    #
+    #         # best line fit
+    #         ys = give_me_a_straight_line(stream_data[index][0], stream_data[index][1], stream_data[index][4])
+    #         axs[i, j].plot(stream_data[index][0], ys, 'r--', label='best fit trend-line')
+    #
+    # plt.show()
 
-    # a.setup_plot()
-    # for _ in a.dates:
-    #     a.next_plot()
+    animation = GenAnimation(stream_data)
     plt.show()
 
 
